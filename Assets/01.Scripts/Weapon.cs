7149ff1e0a31c4e67199abf517a61f57e4d43d26
kkjjwwww <kgw2034@gmail.com>
2026-07-13 14:19:28 +0900

    Weapon1.1

--- Assets/01.Scripts/Weapon.cs
+++ Assets/01.Scripts/Weapon.cs
@@ -1,7 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
+
+    public WeaponData weaponData;
+
     public string name;
     public float baseDamage;
     public float baseRange;
@@ -10,6 +14,16 @@ public abstract class Weapon : MonoBehaviour
     public virtual float FinalDamage => baseDamage;
     public virtual float FinalRange => baseRange;
 
+    protected virtual void Start()
+    {
+        if (weaponData != null)
+        {
+            name = weaponData.itemName;
+            baseDamage = weaponData.baseDamage;
+            baseRange = weaponData.baseRange;
+
+        }
+    }
     public abstract void UserWeapon(Vector2 direction);
  
 }
