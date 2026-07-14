fcb6509397ea43473dac77bf988ec77abcb1a005
kkjjwwww <kgw2034@gmail.com>
2026-07-14 17:31:10 +0900

    WeaponDataUpdate

--- Assets/01.Scripts/ScriptableObjects/WeaponData.cs
+++ Assets/01.Scripts/ScriptableObjects/WeaponData.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="NewWeaponData",menuName = "ScriptableObjects/WeaponData")]
+
 public class WeaponData : ItemData
 {
     public float baseDamage;
     public float baseRange;
-    public float attackAngle;
     public float attackCoolDown;
 }
