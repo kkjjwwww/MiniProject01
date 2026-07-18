5e694f7a713159e9c64733c0b529b821119a64cf
kkjjwwww <kgw2034@gmail.com>
2026-07-19 02:26:33 +0900

    Itme Rarity, UI_LevelUp

---
+++ Assets/01.Scripts/UI/UI_LevelUp.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class UI_LevelUp : MonoBehaviour
+{
+    public static UI_LevelUp instance;
+
+    [SerializeField] private GameObject UIPanel;
+    [SerializeField] private List<ItemData> allItems = new List<ItemData>();
+
+    private List<ItemData> currentSelectedCards = new List<ItemData>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void OpenLevelUpWindow()
+    {
+        if (UIPanel == null) return;
+
+        Time.timeScale = 0f;
+        UIPanel.SetActive(true);
+
+        PickRandomRewards();
+    }
+    private void PickRandomRewards()
+    {
+        currentSelectedCards.Clear();
+
+        List<ItemData> shuffleList = new List<ItemData>(allItems);
+
+        // 안전 장치
+        int countToPick = Mathf.Min(3, shuffleList.Count);
+
+        for (int i = 0; i < countToPick; i++)
+        {
+            int randomIndex = Random.Range(0, shuffleList.Count);
+            currentSelectedCards.Add(shuffleList[randomIndex]);
+            shuffleList.RemoveAt(randomIndex); // 중복 제거
+        }
+
+        for (int i = 0; i < currentSelectedCards.Count; i++)
+        {
+            ItemData item = currentSelectedCards[i];
+            Debug.Log($"선택지 {i + 1}: {item.itemName} [{item.itemRarity}] - {item.itemDescription}");
+
+            
+            // 각 카드 텍스트 아이콘 갱신
+        }
+    }
+}
