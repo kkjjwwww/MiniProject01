5c6b7c3e84372471b842dd6d4f227f8542cd211a
kkjjwwww <kgw2034@gmail.com>
2026-07-20 12:58:54 +0900

    Update UI_LevelUpCard.cs

--- Assets/01.Scripts/UI/UI_LevelUpCard.cs
+++ Assets/01.Scripts/UI/UI_LevelUpCard.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,7 +14,10 @@ public class UI_LevelUpCard : MonoBehaviour
     public void TextSet(ItemData itemData)
     {
         if (itemData == null) return;
+
         assingedItem = itemData;
+
+        SetNameWithLevel(itemData);
 
         nameText.text = itemData.itemName;
         decriptionText.text = itemData.itemDescription;
@@ -28,6 +32,31 @@ public class UI_LevelUpCard : MonoBehaviour
             case ItemRarity.Rare: nameText.color = new Color(65f,105f,225f); break;
             case ItemRarity.Unique: nameText.color = new Color(255f, 215f, 0); break;
             case ItemRarity.Legendary: nameText.color = new Color(255f, 69f, 0); break;
+        }
+    }
+    //중복아이템 드롭시 레벨업 표시
+    private void SetNameWithLevel(ItemData itemData)
+    {
+        int nextLevel = 1;
+        bool isOwned = false;
+
+        if (InventoryManager.instance != null)
+        {
+            Artifact existingItem = InventoryManager.instance.equippedItems.Find(item => item.artifactData.itemID == itemData.itemID);
+
+            if (existingItem != null)
+            {
+                isOwned = true;
+                nextLevel = existingItem.currentLevel + 1;
+            }
+        }
+        if (isOwned)
+        {
+            nameText.text = $"{itemData.name} +{nextLevel}";
+        }
+        else
+        {
+            nameText.text = itemData.name;
         }
     }
 }
