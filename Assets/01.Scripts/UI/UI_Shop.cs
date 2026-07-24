75b4ded99f12735a264fd36c1653023f884325d0
kkjjwwww <kgw2034@gmail.com>
2026-07-24 15:17:40 +0900

    UI.Shop.cs

---
+++ Assets/01.Scripts/UI/UI_Shop.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_Shop : MonoBehaviour
+{
+    [SerializeField] private Transform contentBox;
+    [SerializeField] private UI_ShopStatSlot itemSlotPrefab;
+    [SerializeField] private TMP_Text totalCurrencyText;
+
+    private List<UI_ShopStatSlot> spawnedSlots = new List<UI_ShopStatSlot>();
+
+    private void OnEnable()
+    {
+        RefreshUI();
+    }
+
+    public void RefreshUI()
+    {
+        if (CurrencyManager.instance != null && totalCurrencyText != null)
+        {
+            totalCurrencyText.text = $"보유 군량 : {CurrencyManager.instance.currentCurrency}";
+        }
+        if (spawnedSlots.Count == 0 )
+        {
+            foreach (Transform child in contentBox)
+            {
+                Destroy(child.gameObject);
+            }
+            foreach (var statData in ShopManager.instance.allShopStats)
+            {
+                UI_ShopStatSlot slot = Instantiate(itemSlotPrefab, contentBox);
+                slot.Init(statData, RefreshUI);
+                spawnedSlots.Add(slot);
+            }
+        }
+        else
+        {
+            foreach (var slot in  spawnedSlots)
+            {
+                slot.UpdateUI();
+            }
+        }
+    }
+}
